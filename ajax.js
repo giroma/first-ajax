b4dba3ef72f1deb9f1addef3651d201b69017992
Argir Argirov <dj.giro@gmail.com>
2017-11-14 22:06:29 -0500

    fourth button added and data for location sent to get that timezone

--- ajax.js
+++ ajax.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var rootButton = document.querySelector('.rootButton')
   var secondButton = document.querySelector('.secondButton')
   var thirdButton = document.querySelector('.thirdButton')
+  var fourthButton = document.querySelector('.fourthButton')
 
   rootButton.addEventListener('click', function () {
 
@@ -25,11 +26,9 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log(secondButton); //path[1] selects the parent element
       secondButton.parentNode.innerHTML += `<p>${response}</p>`
 
-    })
-    response.fail(function () {
+    }).fail(function () {
       secondButton.parentNode.append('there was an error')
-    })
-    response.always(function () {
+    }).always(function () {
       console.log('finished');
     })
   })
@@ -45,8 +44,23 @@ document.addEventListener("DOMContentLoaded", function() {
     }).fail(function () {
       thirdButton.parentNode.append('there was an error')
     }).always(function () {
-      console.log('finished');
+      console.log('always');
     })
   })
 
+  fourthButton.addEventListener('click', function () {
+    var response = $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      data: {timezone: 'Europe/Sofia'},
+      dataType: 'text'
+    }).done(function (response) {
+      console.log(response);
+      fourthButton.parentNode.innerHTML += `<p>${response}</p>`
+    }).fail(function () {
+      fourthButton.parentNode.append('there was an error')
+    }).always(function () {
+      console.log('always');
+    })
+  })
 });
